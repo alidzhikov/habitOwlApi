794d0c8b9b8ae01ceb042e5e1721d7f2ba4514f3
m.alidzhikov2630 <m.alidzhikov2630@gmail.com>
2023-09-30 00:18:54 +0300

    BETA - Case sensitive entry

--- api/routes/entry.js
+++ api/routes/entry.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const entryController = require('../controllers/entry');
+const entryController = require('../controllers/Entry');
 
 router.get('/', entryController.getEntries);
 
